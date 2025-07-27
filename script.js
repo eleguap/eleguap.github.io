3c772c13c971b611e3f76ee366d6ab970cbf722a
Andy Yu <andyyu132@gmail.com>
2025-07-26 22:35:38 -0400

    dark home

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
-const text = "Andy Yu";
-const textElement = document.getElementById("text");
-const cursor = document.getElementById("cursor");
-
-// Custom delay for each character (in milliseconds)
-const delays = [1500, 200, 200, 100, 500, 1200, 300, 1500];
-let i = 0;
-
-function type() {
-    if (i < text.length + 2) {
-        if (i > 0 && i <= text.length) {
-            textElement.textContent += text.charAt(i - 1);
-        }
-        setTimeout(type, delays[i]);
-        i++;
-    } else {
-        cursor.style.animation = "none";
-        cursor.style.visibility = "hidden";
-    }
-}
-
-type();
+window.addEventListener('load', () => {
+  document.querySelectorAll('main button').forEach((btn, i) => {
+    setTimeout(() => btn.classList.add('visible'), i * 150); // stagger by 150ms
+  });
+});
